1cc8f2b08d7c145e90c407f23fb49ff2c62df3a9
김형준 <>
2025-12-06 13:52:49 +0900

    feat : MMAP 구현1_do_mmap 함수 구현

--- pintos/include/userprog/process.h
+++ pintos/include/userprog/process.h
@@ -3,22 +3,22 @@
 
 #include "threads/thread.h"
 
-tid_t process_create_initd (const char *file_name);
-tid_t process_fork (const char *name, struct intr_frame *if_);
-int process_exec (void *f_name);
-int process_wait (tid_t);
-void process_exit (void);
-void process_activate (struct thread *next);
-
+tid_t process_create_initd(const char* file_name);
+tid_t process_fork(const char* name, struct intr_frame* if_);
+int process_exec(void* f_name);
+int process_wait(tid_t);
+void process_exit(void);
+void process_activate(struct thread* next);
+bool lazy_load_segment(struct page* page, void* aux);
 #endif /* userprog/process.h */
 
 #ifdef VM
 
 typedef struct lazy_load_info {
-	struct file *file;
-	off_t ofs;
-	size_t page_read_bytes;
-	size_t page_zero_bytes;
+    struct file* file;
+    off_t ofs;
+    size_t page_read_bytes;
+    size_t page_zero_bytes;
 } lazy_load_info;
 
 #endif
--- pintos/vm/file.c
+++ pintos/vm/file.c
@@ -1,58 +1,99 @@
 /* file.c: Implementation of memory backed file object (mmaped object). */
 
+#include "filesys/file.h"
+
+#include "threads/vaddr.h"
+#include "userprog/process.h"
+#include "vm/file.h"
 #include "vm/vm.h"
 
-static bool file_backed_swap_in (struct page *page, void *kva);
-static bool file_backed_swap_out (struct page *page);
-static void file_backed_destroy (struct page *page);
+void* do_mmap(void* addr, size_t length, int writable, struct file* file, off_t offset);
+
+static bool file_backed_swap_in(struct page* page, void* kva);
+static bool file_backed_swap_out(struct page* page);
+static void file_backed_destroy(struct page* page);
 
 /* DO NOT MODIFY this struct */
 static const struct page_operations file_ops = {
-	.swap_in = file_backed_swap_in,
-	.swap_out = file_backed_swap_out,
-	.destroy = file_backed_destroy,
-	.type = VM_FILE,
+    .swap_in = file_backed_swap_in,
+    .swap_out = file_backed_swap_out,
+    .destroy = file_backed_destroy,
+    .type = VM_FILE,
 };
 
 /* The initializer of file vm */
-void
-vm_file_init (void) {
-}
+void vm_file_init(void) {}
 
 /* Initialize the file backed page */
-bool
-file_backed_initializer (struct page *page, enum vm_type type, void *kva) {
-	/* Set up the handler */
-	page->operations = &file_ops;
+bool file_backed_initializer(struct page* page, enum vm_type type, void* kva) {
+    /* Set up the handler */
+    page->operations = &file_ops;
 
-	struct file_page *file_page = &page->file;
+    struct file_page* file_page = &page->file;
 }
 
 /* Swap in the page by read contents from the file. */
-static bool
-file_backed_swap_in (struct page *page, void *kva) {
-	struct file_page *file_page UNUSED = &page->file;
+static bool file_backed_swap_in(struct page* page, void* kva) {
+    struct file_page* file_page UNUSED = &page->file;
 }
 
 /* Swap out the page by writeback contents to the file. */
-static bool
-file_backed_swap_out (struct page *page) {
-	struct file_page *file_page UNUSED = &page->file;
+static bool file_backed_swap_out(struct page* page) {
+    struct file_page* file_page UNUSED = &page->file;
 }
 
 /* Destory the file backed page. PAGE will be freed by the caller. */
-static void
-file_backed_destroy (struct page *page) {
-	struct file_page *file_page UNUSED = &page->file;
+static void file_backed_destroy(struct page* page) {
+    struct file_page* file_page UNUSED = &page->file;
 }
 
 /* Do the mmap */
-void *
-do_mmap (void *addr, size_t length, int writable,
-		struct file *file, off_t offset) {
+void* do_mmap(void* addr, size_t length, int writable, struct file* file, off_t offset) {
+    void* org_addr = addr;
+    struct file* reopen_file = NULL;
+    size_t total_length = file_length(file);
+    struct file_page* file_aux = NULL;
+    for (size_t i = 0; i < length; i += PGSIZE)
+        if (spt_find_page(&thread_current()->spt, addr + i) != NULL)
+            return NULL;
+
+    reopen_file = file_reopen(file);
+    if (reopen_file == NULL)
+        return NULL;
+
+    size_t read_bytes = (length > total_length) ? total_length : length;
+    size_t zero_bytes = length - read_bytes;
+
+    while (read_bytes > 0 || zero_bytes > 0) {
+        // 한 페이지 당 읽을 bytes 양
+        size_t page_read_bytes = (read_bytes < PGSIZE) ? read_bytes : PGSIZE;
+        // 한 페이지 당 0으로 채울 bytes 양
+        size_t page_zero_bytes = PGSIZE - page_read_bytes;
+
+        // aux 할당 및 초기화
+        file_aux = malloc(sizeof(struct file_page));
+        if (file_aux == NULL)
+            return NULL;
+
+        file_aux->file = file_aux;
+        file_aux->page_read_bytes = page_read_bytes;
+        file_aux->page_zero_bytes = page_zero_bytes;
+        file_aux->ofs = offset;
+
+        if (!vm_alloc_page_with_initializer(VM_FILE, addr, writable, lazy_load_segment, file_aux)) {
+            free(file_aux);
+            return NULL;
+        }
+
+        read_bytes -= page_read_bytes;
+        zero_bytes -= page_zero_bytes;
+
+        addr += PGSIZE;
+        offset += page_read_bytes;
+    }
+
+    return org_addr;
 }
 
 /* Do the munmap */
-void
-do_munmap (void *addr) {
-}
+void do_munmap(void* addr) {}
